36ac03880a7c598120309ece03cd0bb23bc6b06e
Stefan Eilemann <eilemann@gmail.com>
2012-03-22 15:43:44 +0100

    Windows build fix

--- lunchbox/api.h
+++ lunchbox/api.h
@@ -1,5 +1,5 @@
  
-/* Copyright (c) 2005-2011, Stefan Eilemann <eile@equalizergraphics.com> 
+/* Copyright (c) 2005-2012, Stefan Eilemann <eile@equalizergraphics.com> 
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -33,9 +33,9 @@
 #  define EQ_DLLIMPORT
 #endif // _MSC_VER
 
-#if defined(COLLAGE_STATIC)
+#if defined(LUNCHBOX_STATIC)
 #  define COBASE_API
-#elif defined(COLLAGE_SHARED)
+#elif defined(LUNCHBOX_SHARED)
 #  define COBASE_API EQ_DLLEXPORT
 #else
 #  define COBASE_API EQ_DLLIMPORT

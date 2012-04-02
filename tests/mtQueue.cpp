8fba8309b891a9ec72e89264799eec42fcc75ec1
Stefan Eilemann <eilemann@gmail.com>
2012-04-02 16:30:36 +0200

    EQ_->LB_ renaming

--- tests/mtQueue.cpp
+++ tests/mtQueue.cpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2010-2011, Stefan Eilemann <eile@equalizergraphics.com>
+/* Copyright (c) 2010-2012, Stefan Eilemann <eile@equalizergraphics.com>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -27,7 +27,7 @@
 
 lunchbox::MTQueue< uint64_t > queue;
 
-#ifdef EQ_GCC_4_6_OR_LATER
+#ifdef LB_GCC_4_6_OR_LATER
 #  pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
 class ReadThread : public lunchbox::Thread

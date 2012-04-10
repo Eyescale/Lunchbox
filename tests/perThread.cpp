b5084d25d6555be6912607c0e0b0851f7bb97029
Stefan Eilemann <eilemann@gmail.com>
2012-04-10 11:07:24 +0200

    Minor test tweak

--- tests/perThread.cpp
+++ tests/perThread.cpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2011, Stefan Eilemann <eile@eyescale.ch>
+/* Copyright (c) 2011-2012, Stefan Eilemann <eile@eyescale.ch>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -71,7 +71,7 @@ int main( int argc, char **argv )
     for( size_t i=0; i<NTHREADS; ++i )
         TEST( threads[i].join( ));
 
-    TEST( _threads == 0 );
+    TESTINFO( _threads == 0, _threads );
     TEST( lunchbox::exit( ));
     return EXIT_SUCCESS;
 }

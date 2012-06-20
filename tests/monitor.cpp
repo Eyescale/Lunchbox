0c36fd6ed8c816df95522fc6b8dbf9dabf72cc7b
Stefan Eilemann <eilemann@gmail.com>
2012-06-20 15:40:52 +0200

    Unit test for #1

--- tests/monitor.cpp
+++ tests/monitor.cpp
@@ -4,12 +4,12 @@
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
  * by the Free Software Foundation.
- *  
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -21,11 +21,16 @@
 #include <lunchbox/clock.h>
 #include <lunchbox/monitor.h>
 #include <lunchbox/thread.h>
+#include <lunchbox/uint128_t.h>
 #include <iostream>
 
 #define NLOOPS 200000
 
+using lunchbox::uint128_t;
+
 lunchbox::Monitor< int64_t > monitor;
+lunchbox::Monitor< uint128_t > issue1;
+const uint128_t big( 10, 10 );
 
 class Thread : public lunchbox::Thread
 {
@@ -47,11 +52,31 @@ public:
         }
 };
 
+class Issue1 : public lunchbox::Thread
+{
+public:
+    virtual ~Issue1() {}
+    virtual void run()
+        {
+            const uint128_t invalid1( 0, 0 );
+            const uint128_t invalid2( 1, 1 );
+
+            while( issue1 != big )
+            {
+                const uint128_t& result = issue1.waitLE( big );
+                TEST( result != invalid1 );
+                TEST( result != invalid2 );
+                TEST( issue1 != invalid1 );
+                TEST( issue1 != invalid2 );
+            }
+        }
+};
+
 int main( int argc, char **argv )
 {
     Thread waiter;
     int64_t nOps = NLOOPS;
-    
+
     TEST( waiter.start( ));
     lunchbox::Clock clock;
 
@@ -65,6 +90,22 @@ int main( int argc, char **argv )
 
     TEST( waiter.join( ));
     std::cout << 2*NLOOPS/time << " ops/ms" << std::endl;
+
+    const uint128_t valid1( 1, 0 );
+    const uint128_t valid2( 0, 1 );
+
+    issue1 = valid1;
+    nOps = NLOOPS;
+    Issue1 thread;
+
+    TEST( thread.start( ));
+    while( --nOps )
+    {
+        issue1 = valid1;
+        issue1 = valid2;
+    }
+    issue1 = big;
+    TEST( thread.join( ));
+
     return EXIT_SUCCESS;
 }
-

9dd78e28788b3858dff36ba99225666d1f20f167
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-03-11 11:21:01 +0100

    Release preparations

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -7,10 +7,10 @@ mark_as_advanced(LUNCHBOX_PACKAGE_VERSION)
 set(CPACK_PACKAGE_VENDOR "www.eyescale.ch")
 set(CPACK_PACKAGE_CONTACT "Stefan Eilemann <eile@eyescale.ch>")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Multi-threaded toolbox library")
-set(CPACK_DEBIAN_BUILD_DEPENDS libboost-serialization-dev
+set(CPACK_DEBIAN_BUILD_DEPENDS libboost-regex-dev libboost-serialization-dev
   libavahi-compat-libdnssd-dev)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
-  "libstdc++6, libboost-serialization-dev, libavahi-compat-libdnssd1")
+  "libstdc++6, libboost-regex-dev, libboost-serialization-dev, libavahi-compat-libdnssd1")
 
 set(CPACK_MACPORTS_CATEGORY devel)
 set(CPACK_MACPORTS_DEPENDS boost avahi)

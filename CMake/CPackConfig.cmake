143809b7b78aededc120ea72c2a756c45f873fc2
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-04-23 16:58:15 +0200

    Update package dependencies

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -11,9 +11,9 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
 
 set(CPACK_DEBIAN_BUILD_DEPENDS libboost-regex-dev libboost-serialization-dev
-  libavahi-compat-libdnssd-dev)
+  libavahi-compat-libdnssd-dev libhwloc-dev)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
-  "libstdc++6, libboost-regex-dev, libboost-serialization-dev, libavahi-compat-libdnssd1")
+  "libstdc++6, libboost-regex-dev, libboost-serialization-dev, libavahi-compat-libdnssd1, libhwloc-dev")
 
 set(CPACK_MACPORTS_CATEGORY devel)
 set(CPACK_MACPORTS_DEPENDS boost avahi)

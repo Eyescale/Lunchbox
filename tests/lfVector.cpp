811ef1b3b3431c58b56182dbf941020f746e4584
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-11-07 16:08:59 +0100

    Increase test runtimes for, you guessed it, Travis. *GRR*

--- tests/lfVector.cpp
+++ tests/lfVector.cpp
@@ -16,7 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#define TEST_RUNTIME 300 // seconds
+#define TEST_RUNTIME 600 // seconds
 #include "test.h"
 
 #pragma warning (push)

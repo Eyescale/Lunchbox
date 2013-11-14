75af4679d0bed8e9164e84e7563d054cd87892c7
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-11-14 16:03:09 +0100

    More tries to get Travis running...

--- CMake/CommonCTest.cmake
+++ CMake/CommonCTest.cmake
@@ -22,7 +22,6 @@ if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
   endif()
   if($ENV{TRAVIS})
     message(STATUS "Disable code coverage on Travis, causes build timeouts")
-  endif()
   elseif(LCOV AND GENHTML)
     set(COVERAGE ON)
     add_custom_target(lcov-clean

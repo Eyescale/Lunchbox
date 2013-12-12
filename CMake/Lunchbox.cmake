7ced4435429d6719a6b4ec1ec506497a419a5876
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-12-12 08:18:07 +0100

    Update generated files

--- CMake/Lunchbox.cmake
+++ CMake/Lunchbox.cmake
@@ -1,6 +1,6 @@
 
 
-set(LUNCHBOX_PACKAGE_VERSION 1.8.0)
+set(LUNCHBOX_PACKAGE_VERSION 1.8.1)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG 1.8)
 set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD REQUIRED Boost)

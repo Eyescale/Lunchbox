11df1408330f0669d4faebc29b67c49c46702148
Ahmet Bilgili <ahmet.bilgili@epfl.ch>
2014-06-16 16:02:27 +0200

    Fix dependencies for Equalizer build process

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -10,10 +10,11 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
 set(CPACK_RESOURCE_FILE_README ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
 
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
-  "libstdc++6, libboost-regex-dev, libboost-serialization-dev, libavahi-compat-libdnssd1, libhwloc-dev")
+  "libstdc++6, libboost-regex-dev, libboost-serialization-dev, libavahi-compat-libdnssd1, libhwloc-dev, libturbojpeg")
 
 set(CPACK_MACPORTS_CATEGORY devel)
 set(CPACK_MACPORTS_DEPENDS boost)
 
 include(CommonCPack)
 include(OSSCPack)
+

2043d8e4758ed599c274484abd5a4a93aa12a0a9
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-09-19 14:10:13 +0200

    Update generated files

--- CMake/Lunchbox.cmake
+++ CMake/Lunchbox.cmake
@@ -12,6 +12,7 @@ set(LUNCHBOX_BOOST_COMPONENTS "regex serialization filesystem system thread")
 set(LUNCHBOX_MATURITY RD)
 set(LUNCHBOX_FORCE_BUILD ${CI_BUILD})
 set(LUNCHBOX_SUBPROJECT ON)
+
 if(CI_BUILD_COMMIT)
   set(LUNCHBOX_REPO_TAG ${CI_BUILD_COMMIT})
 else()

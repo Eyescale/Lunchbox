a20e2a2e0c7fd4bf2e211aa85e3a68e581297cf1
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2014-10-08 14:32:39 +0200

    Update generated files

--- CMake/Lunchbox.cmake
+++ CMake/Lunchbox.cmake
@@ -1,12 +1,13 @@
 
 
-set(LUNCHBOX_PACKAGE_VERSION 1.9)
+set(LUNCHBOX_PACKAGE_VERSION 1.10)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD avahi-client
   LibJpegTurbo OpenMP MPI leveldb skv REQUIRED Boost)
 set(LUNCHBOX_DEB_DEPENDS libboost-regex-dev libboost-serialization-dev
-  libboost-filesystem-dev libboost-system-dev libboost-thread-dev
-  libhwloc-dev libavahi-client-dev libjpeg-turbo8-dev libturbojpeg libleveldb-dev)
+  libboost-filesystem-dev libboost-system-dev libboost-thread-dev libhwloc-dev
+  avahi-daemon libavahi-client-dev libjpeg-turbo8-dev libturbojpeg libleveldb-dev
+  libopenmpi-dev openmpi-bin)
 set(LUNCHBOX_PORT_DEPENDS boost)
 set(LUNCHBOX_BOOST_COMPONENTS "regex serialization filesystem system thread")
 set(LUNCHBOX_MATURITY RD)

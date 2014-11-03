7ecf3eb40f36588e3600e4c5c79e9c3dc665eac3
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-11-03 12:37:29 +0100

    Another skv link fix

--- CMake/Lunchbox.cmake
+++ CMake/Lunchbox.cmake
@@ -5,11 +5,12 @@ set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD avahi-client
   LibJpegTurbo OpenMP MPI leveldb skv REQUIRED Boost)
 set(LUNCHBOX_DEB_DEPENDS libboost-regex-dev libboost-serialization-dev
-  libboost-filesystem-dev libboost-system-dev libboost-thread-dev libhwloc-dev
-  avahi-daemon libavahi-client-dev libjpeg-turbo8-dev libturbojpeg libleveldb-dev
-  libopenmpi-dev openmpi-bin)
+  libboost-filesystem-dev libboost-system-dev libboost-test-dev
+  libboost-thread-dev libhwloc-dev avahi-daemon libavahi-client-dev
+  libjpeg-turbo8-dev libturbojpeg libleveldb-dev libopenmpi-dev openmpi-bin)
 set(LUNCHBOX_PORT_DEPENDS boost)
-set(LUNCHBOX_BOOST_COMPONENTS "regex serialization filesystem system thread")
+set(LUNCHBOX_BOOST_COMPONENTS
+  "regex serialization filesystem system thread unit_test_framework")
 set(LUNCHBOX_MATURITY RD)
 set(LUNCHBOX_FORCE_BUILD ${CI_BUILD})
 

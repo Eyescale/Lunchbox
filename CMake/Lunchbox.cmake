bc19d07077aef72644fe50fca623d8d0da4cd37a
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-04-13 15:08:04 +0200

    Single CMake/common superproject

--- CMake/Lunchbox.cmake
+++ CMake/Lunchbox.cmake
@@ -1,5 +1,4 @@
 
-
 set(LUNCHBOX_PACKAGE_VERSION 1.10)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_DEPENDS eyescale eyescalePorts hwloc DNSSD avahi-client
@@ -12,7 +11,7 @@ set(LUNCHBOX_PORT_DEPENDS boost)
 set(LUNCHBOX_BOOST_COMPONENTS
   "regex serialization filesystem system thread unit_test_framework")
 set(LUNCHBOX_MATURITY RD)
-set(LUNCHBOX_FORCE_BUILD ${CI_BUILD})
+set(LUNCHBOX_SUBPROJECT ON)
 
 if(CI_BUILD_COMMIT)
   set(LUNCHBOX_REPO_TAG ${CI_BUILD_COMMIT})

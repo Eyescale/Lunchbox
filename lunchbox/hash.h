ac09257ce9465a8de1d0d6823e01d44299ac0c5e
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2017-02-08 16:43:29 +0100

    Remove C++03 zombies

--- lunchbox/hash.h
+++ lunchbox/hash.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2005-2012, Stefan Eilemann <eile@equalizergraphics.com>
+/* Copyright (c) 2005-2017, Stefan Eilemann <eile@equalizergraphics.com>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -19,67 +19,30 @@
 #define LUNCHBOX_HASH_H
 
 #include <lunchbox/refPtr.h>
-#include <lunchbox/stdExt.h>
+
+#include <unordered_map>
 
 namespace lunchbox
 {
 /** A hash for pointer keys. @version 1.0 */
 template <class K, class T>
-class PtrHash
-#ifdef _MSC_VER
-    : public stde::hash_map<K, T, stde::hash_compare<const void*> >
-#else
-    : public stde::hash_map<K, T, stde::hash<const void*> >
-#endif
+class PtrHash : public std::unordered_map<K, T, std::hash<const void*> >
 {
 };
 
-/** A hash function for RefPtr keys. @version 1.0 */
-#ifdef _MSC_VER
-template <typename T>
-class hashRefPtr : public stde::hash_compare<RefPtr<T> >
-{
-public:
-    size_t operator()(const RefPtr<T>& key) const
-    {
-        stde::hash_compare<const void*> comp;
-        return comp(key.get());
-    }
-
-    bool operator()(const RefPtr<T>& k1, const RefPtr<T>& k2) const
-    {
-        return k1 < k2;
-    }
-};
-
-template <class K, class T>
-class RefPtrHash : public stde::hash_map<RefPtr<K>, T, hashRefPtr<K> >
-{
-};
-
-#else
 template <typename T>
 struct hashRefPtr
 {
     size_t operator()(RefPtr<T> key) const
     {
-        return stde::hash<const void*>()(key.get());
+        return std::hash<const void*>()(key.get());
     }
 };
 
 /** A hash for RefPtr keys. @version 1.0 */
 template <class K, class T>
-class RefPtrHash : public stde::hash_map<RefPtr<K>, T, hashRefPtr<K> >
+class RefPtrHash : public std::unordered_map<RefPtr<K>, T, hashRefPtr<K> >
 {
 };
-#endif
-
-#ifdef LUNCHBOX_USE_V1_API
-/** A hash for UUID keys. @version 1.0 */
-template <class T>
-class UUIDHash : public stde::hash_map<lunchbox::UUID, T>
-{
-};
-#endif
 }
 #endif // LUNCHBOX_HASH_H
